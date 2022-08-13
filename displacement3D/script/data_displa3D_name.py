49db9ba44c3c2a3937576b4f90d9c3f411b27b3d
Julita Inca <jinca@pucp.pe>
2022-08-13 19:00:55 +0100

    Adding the files for the Displa3D experience

---
+++ displacement3D/script/data_displa3D_name.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+plt.rcParams.update({'font.size': 15})
+
+df = pd.read_csv("data_displa3D_name.csv")
+
+fig, ax = plt.subplots(2, sharex=True)
+
+sns.boxplot(x="mpi", y="fep", hue="opt_name", data=df, ax=ax[0])
+sns.boxplot(x="mpi", y="total", hue="opt_name", data=df, ax=ax[1])
+ax[0].set_ylabel("FE time (s)")
+ax[0].set_xlabel("MPI tasks (n)",fontdict={"fontsize":16})
+ax[1].set_ylabel("Total Time (s)")
+ax[1].set_xlabel("MPI tasks (n)",fontdict={"fontsize":16})
+ax[0].legend()
+ax[1].legend()
+plt.show()
+
